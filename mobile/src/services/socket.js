4c3e02cf7a710b2e0dfb321f429b26ab35e3d872
castelogui <guilhermerafaelcrisostomocaste@gmail.com>
2020-04-03 23:58:18 -0400

    Atualização

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,6 +1,6 @@
 import socketio from 'socket.io-client';
 
-const socket = socketio('http://192.168.1.8:3333', {
+const socket = socketio('http://192.168.2.17:3333', {
     autoConnect: false,
 });
 
